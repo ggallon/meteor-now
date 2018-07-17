f8a80b14f4667446de4e23dce3f7f64c7d154b08
Gwenaël Galon <gwenael.gallon@mac.com>
2018-07-17 23:47:24 +0200

    Rename package to rock-now

--- src/api/constants.js
+++ src/api/constants.js
@@ -15,4 +15,4 @@ export const meteorNowBuildPath = isWin ? `${homePath}\\.meteor-now\\build` : `$
 export const tarFileName = `${projectName}.tar.gz`;
 export const ignoreVarsArray = ['MONGO_URL', 'ROOT_URL', 'METEOR_SETTINGS', 'PORT'];
 export const ignoreOptionsArray = ['deps', '_', '$0', 'help', 'version', 'nosplit'];
-export const logPrefix = '[METEOR-NOW] - ';
+export const logPrefix = '[ROCK-NOW] - ';
